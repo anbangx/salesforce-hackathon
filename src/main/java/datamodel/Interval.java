e7d3a33609d093f2812e4280b2a0720d8754e85f
xwang2014 <x.wang2014@gmail.com>
2014-10-11 20:18:30 -0700

    Adjust Task table due to Interval change

--- src/main/java/datamodel/Interval.java
+++ src/main/java/datamodel/Interval.java
@@ -2,8 +2,18 @@ package datamodel;
 
 public class Interval {
 	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return this.start + ":" + this.end;
+	}
+
+
+
 	public long start;
 	public long end;
+	
+	
 	
 	public Interval(long start, long end){
 		this.start = start;
